d8c2d8ecd623da3b6b8408c5dfa30e7ee3bb6a94
Otty <otty.praveen@gmail.com>
2022-06-25 22:55:25 +0530

    This is my first Comment

---
+++ src/test/java/Academy/TestCase12.java
@@ -0,0 +1,12 @@
+package Academy;
+
+public class TestCase12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hey World This is Praveen");
+
+	}
+
+}
