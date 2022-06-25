7f21bcfde564bd6122dfbfbc11c6a5cc58cd6162
Otty <otty.praveen@gmail.com>
2022-06-25 23:07:32 +0530

    thisis first Change by Asian Guy 1

--- src/test/java/Academy/TestCase10.java
+++ src/test/java/Academy/TestCase10.java
@@ -6,6 +6,7 @@ public class TestCase10 {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World");
+		System.out.println("Hello God");
 
 	}
 
--- src/test/java/Academy/TestCase11.java
+++ src/test/java/Academy/TestCase11.java
@@ -5,6 +5,7 @@ public class TestCase11 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bye Wordgit");
+		System.out.println("BMW");
 
 	}
 
